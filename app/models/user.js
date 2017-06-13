dec90c753c7362bd880ee81e0e187de4cafb6f97
Purva Singh <singhpurva2906@gmail.com>
2017-06-13 08:11:11 +0530

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,25 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-  username: {type: String, lowercase: true, required: true, unique: true},
-  password: {type: String, lowercase: true, required: true, unique: true}
+  username: {type: String,  required: true, unique: true},
+  password: {type: String,  required: true, unique: true}
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, null, null, function(err, hash){
+  	if(err) return next(err);
+  	user.password=hash;
+  	next();
+  });
+  
 });
 
 
+UserSchema.methods.comparePassword = function(password){
+	return bcrypt.compareSync(password, this.password);
+};
+
 module.exports = mongoose.model('User', UserSchema); 
+
