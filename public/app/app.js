c9e480e2a15236c0392f0ed996da684bf8d9c328
Purva Singh <singhpurva2906@gmail.com>
2017-06-13 08:08:18 +0530

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,2 +1,6 @@
-var userApp = angular.module('userApp', ['appRoute', 'userControllers', 'userServices']);
+var userApp = angular.module('userApp', ['appRoute', 'userControllers', 'userServices', 'mainController', 'authServices'])
 
+.config(function($httpProvider){
+	//configuring app to all http requests
+	$httpProvider.interceptors.push('AuthInterceptors');
+});
