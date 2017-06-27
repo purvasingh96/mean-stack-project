16e397656557aa372de32b45ed40fe93e26e955a
Purva Singh <singhpurva2906@gmail.com>
2017-06-27 15:54:51 +0530

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var userApp = angular.module('userApp', ['appRoute', 'userControllers', 'userServices', 'mainController', 'authServices', 'csvController'])
+var userApp = angular.module('userApp', ['appRoute', 'userControllers', 'userServices', 'mainController', 'authServices', 'studentServices', 'csvController', 'managementController', 'studentServices'])
 
 .config(function($httpProvider){
 	//configuring app to all http requests
